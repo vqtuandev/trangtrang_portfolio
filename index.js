db31241feebd38451b89589e206a24926f76a39d
TUANVO <admin@ADmins-MacBook-Pro.local>
2020-03-14 14:51:40 +0700

    verify ssl

--- index.js
+++ index.js
@@ -11,7 +11,14 @@ app.use(express.static('public'));
 //sfes
 app.get('/', (req, res)=>{
     res.render('trangchu');
-})
+});
+
+app.get('/.well-known/acme-challenge/j8cMIplL2UVrUwboQ3Jl1f0ZX5yaovt22_ZdO1hwGwY', (req, res) => {
+    res.send("j8cMIplL2UVrUwboQ3Jl1f0ZX5yaovt22_ZdO1hwGwY.Ph8VGD33Ji6s5EMpQMgpzyqBmck8sFbP6iW2t94IOnY")
+});
+app.get('/.well-known/acme-challenge/NUxnSGD0VgXQAsdVXhisnunAKyvBJDT27MkclM0eomo', (req, res) => {
+    res.send("NUxnSGD0VgXQAsdVXhisnunAKyvBJDT27MkclM0eomo.Ph8VGD33Ji6s5EMpQMgpzyqBmck8sFbP6iW2t94IOnY")
+});
 
 app.get('*', (req, res)=>{
     res.redirect('/');
